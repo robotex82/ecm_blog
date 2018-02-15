65c65d699a6153dc5ccbe9b12a5881a92b9f13af
Roberto Vasquez Angel <roberto@vasquez-angel.de>
2018-02-15 21:23:12 +0100

    Make base controller class name configurable in the install generator via BASE_CONTROLLER_CLASS_NAME option.

--- lib/generators/ecm/blog/install/templates/initializer.rb
+++ lib/generators/ecm/blog/install/templates/initializer.rb
@@ -1,9 +1,9 @@
 Ecm::Blog.configure do |config|
-  # Set the base controller for the controllers
+  # Set the base controller for the page controller
   #
-  # Default: config.base_controller = '::FrontendController'
+  # Default: config.base_controller = '<%= base_controller_class_name %>'
   #
-  config.base_controller = '::FrontendController'
+  config.base_controller = '<%= base_controller_class_name %>'
   
   config.creator_class_name = 'User'
 end
